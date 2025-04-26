4d06424ce40480dd3e84899a459964948c5b2c03
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-26 19:31:45 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,56 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/CCEGLView.hpp>
 #include <Windows.h>
 
 using namespace geode::prelude;
 
-WNDPROC g_originalWndProc = nullptr;
+HHOOK g_keyboardHook = nullptr;
 
-LRESULT CALLBACK CustomWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    if (message == WM_KEYDOWN) {
-        if (wParam == VK_LWIN || wParam == VK_RWIN) {
-            ShowWindow(hWnd, SW_MINIMIZE);
-            return 0;
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode == HC_ACTION) {
+        KBDLLHOOKSTRUCT* pKeyInfo = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+        
+        if (wParam == WM_KEYDOWN && (pKeyInfo->vkCode == VK_LWIN || pKeyInfo->vkCode == VK_RWIN)) {
+            HWND hWnd = FindWindow(L"CocosDenshion", nullptr);
+            if (!hWnd) {
+                hWnd = FindWindow(L"GLFW30", nullptr);
+            }
+            
+            if (hWnd) {
+                if (GetForegroundWindow() == hWnd) {
+                    ShowWindow(hWnd, SW_MINIMIZE);
+                    return 1;
+                }
+            }
         }
     }
     
-    return CallWindowProc(g_originalWndProc, hWnd, message, wParam, lParam);
+    return CallNextHookEx(g_keyboardHook, nCode, wParam, lParam);
 }
 
-class $modify(CCEGLView) {
-    bool $modify(initWithRect)(RECT rect, float scaleFactor, bool isFullscreen) {
-        auto result = this->$original(rect, scaleFactor, isFullscreen);
-        
-        if (result) {
-            HWND hWnd = nullptr;
-            
-            hWnd = static_cast<HWND>(CCDirector::sharedDirector()->getOpenGLView()->getWindow());
-            
-            if (hWnd) {
-                g_originalWndProc = reinterpret_cast<WNDPROC>(
-                    SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(CustomWndProc))
-                );
-                
-                log::info("Win Minimizer mod initialized: Window procedure hook established");
-            } else {
-                log::error("Win Minimizer mod: Failed to get window handle");
-            }
-        }
-        
-        return result;
+$on_mod(Loaded) {
+    g_keyboardHook = SetWindowsHookEx(
+        WH_KEYBOARD_LL,
+        LowLevelKeyboardProc,
+        GetModuleHandle(NULL),
+        0
+    );
+    
+    if (g_keyboardHook) {
+        log::info("Yippie sussces");
+    } else {
+        log::error("Error idk", GetLastError());
     }
-};
+    
+    return true;
+}
+
+// Clean up hook when the mod is unloaded
+$on_mod(Unloaded) {
+    if (g_keyboardHook) {
+        UnhookWindowsHookEx(g_keyboardHook);
+        g_keyboardHook = nullptr;
+        log::info("Win Minimizer mod: Keyboard hook removed");
+    }
+    
+    return true;
+}
