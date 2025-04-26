361532df17df6090c3882dcd581cf7eec9a842b8
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-26 16:56:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,8 @@
 
 using namespace geode::prelude;
 
-class AutoMinimize : public geode::Mod {
-public:
-    void onLoad() override {
-        CCDirector::sharedDirector()->getScheduler()->scheduleSelector(
-            schedule_selector(+[](float) {
-                checkForWindowsKey();
-            }),
-            CCDirector::sharedDirector(),
-            0.1f,
-            false
-        );
-    }
-
-    static void checkForWindowsKey() {
+namespace {
+    void checkForWindowsKey(float) {
         if (isFullscreenBorderless()) {
             if (GetAsyncKeyState(VK_LWIN) & 0x8000 || GetAsyncKeyState(VK_RWIN) & 0x8000) {
                 minimizeWindow();
@@ -24,17 +12,26 @@ public:
         }
     }
 
-    static bool isFullscreenBorderless() {
+    bool isFullscreenBorderless() {
         auto settings = GameManager::sharedState();
         bool isFullscreen = settings->getGameVariable("0010"); // fullscreen
         bool isBorderless = settings->getGameVariable("0040"); // borderless
         return isFullscreen && isBorderless;
     }
 
-    static void minimizeWindow() {
+    void minimizeWindow() {
         HWND hwnd = GetActiveWindow();
         if (hwnd) {
             ShowWindow(hwnd, SW_MINIMIZE);
         }
     }
-};
+}
+
+$on_mod(Loaded) {
+    CCDirector::sharedDirector()->getScheduler()->scheduleSelector(
+        schedule_selector(checkForWindowsKey),
+        CCDirector::sharedDirector(),
+        0.1f,
+        false
+    );
+}
